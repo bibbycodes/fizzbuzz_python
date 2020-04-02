810bf188d7016e2a1f2c3c9be59864dc41677132
Robert <bibbycodes@gmail.com>
2020-04-02 11:45:44 +0100

    fizz and buzz

--- lib/fizzbuzz.py
+++ lib/fizzbuzz.py
@@ -1,2 +1,9 @@
 def fizzbuzz(input):
-  return input
+  if input == 0:
+    return input
+  elif input % 3 == 0:
+    return 'fizz'
+  elif input == 5:
+    return 'buzz'
+  else:
+    return input
--- tests/fizzbuzz_test.py
+++ tests/fizzbuzz_test.py
@@ -5,3 +5,18 @@ def test_fizzbuzz_with_input_0():
 
 def test_fizzbuzz_with_input_1():
   assert fizzbuzz(1) == 1
+
+def test_fizzbuzz_with_input_2():
+    assert fizzbuzz(2) == 2
+
+def test_fizzbuzz_with_input_3():
+  assert fizzbuzz(3) == 'fizz'
+
+def test_fizzbuzz_with_input_4():
+  assert fizzbuzz(4) == 4
+
+def test_fizzbuzz_with_input_5():
+  assert fizzbuzz(5) == "buzz"
+
+def test_fizzbuzz_with_input_6():
+  assert fizzbuzz(6) == 'fizz'
